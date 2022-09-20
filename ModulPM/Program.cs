0707823db8c138d3b8163f430cf4cae9cf7a3136
богомазовмв <@101-09>
2022-09-20 16:31:04 +0300

    Добавьте файлы проекта.

---
+++ ModulPM/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModulPM
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!"); //Вывод в консоль Hello world
+            /// Плюс
+            /// баллы
+            /// =)
+        }
+    }
+    class PlusBally // Создание класса Плюс Баллы
+    {
+        public void SayBally()
+        {
+            //Это метод
+            Console.WriteLine(17+29); 
+            ///Вывод сообщения плюс баллы, ну почти
+            ///А это вызов метода
+            SayBally();
+        }
+        
+    }
+
+}
---
+++ UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fe5fb712-20f1-4021-9329-ff02559f3349")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
